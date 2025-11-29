755643aeb514c8c3e3e29ca7f4a133456ed010b7
minty <eithanmathewmalonzo@gmail.com>
2025-11-29 19:14:54 +0800

    Add services section

---
+++ components/ui/service-card.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export interface ServiceCardProps {
+  imgSrc: string;
+  service: string;
+  href: string;
+  span: string;
+  className?: string;
+}
+export default function ServiceCard({
+  imgSrc,
+  service,
+  href,
+  span,
+  className,
+}: ServiceCardProps) {
+  return (
+    <Link
+      href={href}
+      className={`relative block w-fill h-100 ${className} ${span}`}
+    >
+      <Image
+        src={imgSrc}
+        alt={service}
+        fill
+        className="object-cover rounded-4xl"
+      />
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2">
+        <h3 className="text-4xl text-white drop-shadow-2xl drop-shadow-black">
+          {service}
+        </h3>
+      </div>
+      <div className="absolute inset-0 w-full h-full hover:bg-white/10 transition-all duration-500"></div>
+    </Link>
+  );
+}
