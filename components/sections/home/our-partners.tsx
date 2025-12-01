c9c58e9bec2e8ffc24838d2f2c450e1452901f89
minty <eithanmathewmalonzo@gmail.com>
2025-12-01 14:38:06 +0800

    Add the more key partners

--- components/sections/home/our-partners.tsx
+++ components/sections/home/our-partners.tsx
@@ -2,12 +2,46 @@ import Image from "next/image";
 
 export default function OurPartners() {
   const partners = [
-    { name: "LGU", logo: "/assets/lgu-logo.png", isImage: true },
-    { name: "DOST", logo: "/assets/dost-logo.png", isImage: true },
-    { name: "DTI", logo: "/assets/dti-logo.png", isImage: true },
-    { name: "KOICA", logo: "/assets/koica-logo.png", isImage: true },
-    { name: "PCCI", logo: "/assets/pcci-logo.png", isImage: true },
-    { name: "DICT", logo: "/assets/dict-logo.png", isImage: true },
+    {
+      name: "LGU",
+      logo: "/assets/key-partner-logos/lgu-logo.png",
+      isImage: true,
+    },
+    {
+      name: "DOST",
+      logo: "/assets/key-partner-logos/dost-logo.png",
+      isImage: true,
+    },
+    {
+      name: "DTI",
+      logo: "/assets/key-partner-logos/dti-logo.png",
+      isImage: true,
+    },
+    {
+      name: "KOICA",
+      logo: "/assets/key-partner-logos/koica-logo.png",
+      isImage: true,
+    },
+    {
+      name: "PCCI",
+      logo: "/assets/key-partner-logos/pcci-logo.png",
+      isImage: true,
+    },
+    {
+      name: "DICT",
+      logo: "/assets/key-partner-logos/dict-logo.png",
+      isImage: true,
+    },
+    {
+      name: "GUITEB",
+      logo: "/assets/key-partner-logos/guiteb.png",
+      isImage: true,
+    },
+    {
+      name: "Igting",
+      logo: "/assets/key-partner-logos/igting.png",
+      isImage: true,
+    },
     { name: "NGO", isImage: false },
   ];
 
@@ -21,7 +55,7 @@ export default function OurPartners() {
         priority
       />
 
-      <div className="relative z-10 flex flex-col items-center gap-8 w-full max-w-6xl px-8">
+      <div className="relative z-10 flex flex-col items-center gap-8 w-full max-w-7xl px-8">
         <h2 className="text-6xl font-bold text-white text-center">
           Our Partners
         </h2>
