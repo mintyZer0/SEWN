8fc824d02a3e76abc61c93fd4f919e55d2f57e13
minty <eithanmathewmalonzo@gmail.com>
2026-02-21 14:05:17 +0800

    Updated Header to look like the redesign

--- components/ui/search-bar.tsx
+++ components/ui/search-bar.tsx
@@ -1,15 +1,15 @@
 import { Search } from "react-feather";
 export default function SearchBar() {
   return (
-    <div className="relative flex flex-row w-full bg-primary-light h-16 p-3 rounded-2xl gap-2">
-      <span className="flex flex-row items-center text-2xl w-30 gap-4 text-center p-2">
+    <div className="relative flex flex-row w-full bg-[#7B3B7B] h-16 p-3 rounded-2xl">
+      <span className="flex flex-row items-center  font-light text-3xl text-white w-auto h-auto gap-4 text-center p-2">
         <Search />
         Search
       </span>
       <form role="search" className="flex flex-1">
         <input
           type="search"
-          className="flex flex-1 h-auto bg-gray-300 rounded-2xl"
+          className="flex flex-1 h-auto rounded-2xl bg-white p-4"
         />
       </form>
     </div>
