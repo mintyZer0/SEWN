8df98e607face19933a978b13d5c2ab523471a90
minty <eithanmathewmalonzo@gmail.com>
2026-02-25 23:10:28 +0800

    Added working login using google connected to supabase - only used shadcn components, need to customize to follow design - you can retrieve the data from supabase

---
+++ app/(auth)/auth/signup/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { SignupForm } from "./components/sign-up-form";
+
+export default function SignUpPage() {
+  return (
+    <div className="flex h-svh items-center">
+      <SignupForm />
+    </div>
+  );
+}
